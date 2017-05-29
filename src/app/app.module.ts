e3b9b959b0b6471a8418a1b90a615753437abfc5
ARTUR <artur.tovmasyan777@gmail.com>
2017-05-29 18:57:13 +0400

    googlr map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { appRouting } from './app-routing';
+import { UserListComponent } from './user-list/user-list.component';
+import { GoogleMapComponent } from './google-map/google-map.component';
+import { CheckInModalComponent } from './modals/check-in-modal/check-in-modal.component';
+
+import { AgmCoreModule } from 'angular2-google-maps/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    UserListComponent,
+    GoogleMapComponent,
+    CheckInModalComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    appRouting,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCXQuy-ESvHy_6Y2WKb3QCw8GwITh2Fe90'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/google-map/google-map.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-google-map',
+  templateUrl: './google-map.component.html',
+  styleUrls: ['./google-map.component.css']
+})
+export class GoogleMapComponent implements OnInit {
+  lat = 40.3855031;
+  lng = 45.364846;
+  zoom = 10;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.getLocation();
+  }
+
+  /**
+   * This function is used to get current location for user
+   */
+  getLocation() {
+
+    if (navigator.geolocation) {
+
+      navigator.geolocation.getCurrentPosition(
+        (showPosition) => {
+          this.lat = showPosition.coords.latitude;
+          this.lng = showPosition.coords.longitude;
+        },
+
+        (err) => {
+          console.log(err);
+        }
+      );
+    } else {
+      console.log('Your browser don\`t support HTML5');
+    }
+  }
+
+}
+
