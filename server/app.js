c36559775d9ce6bbd833588ab35f9523dd7a9d65
ARTUR <artur.tovmasyan777@gmail.com>
2017-05-31 14:25:52 +0400

    first deploy

--- server/app.js
+++ server/app.js
@@ -13,6 +13,18 @@ process.env.TZ = 'Asia/Yerevan';
 
 const app = express();
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
+
+
 // Add headers
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -38,7 +50,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '../dist'));
+
+app.use(forceSSL());
 
 app.use('/', index);
 app.use('/api/users', users);
