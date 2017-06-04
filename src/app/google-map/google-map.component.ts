6a70f7d525e088898303fab82c62667f9cdc5910
ARTUR <artur.tovmasyan777@gmail.com>
2017-06-04 18:38:56 +0400

    fix

--- src/app/google-map/google-map.component.ts
+++ src/app/google-map/google-map.component.ts
@@ -89,9 +89,9 @@ export class GoogleMapComponent implements OnInit {
     // GENERATE USER DATA AND SAVE IT
     this.user = Object.assign({}, {
       name : data,
-      coordinate : this.coordinate
-      // coordinate : [41.610836, 41.606056]
-      // 41.606056, 41.610836
+      // coordinate : this.coordinate
+      coordinate : [44.437512, 40.148802]
+      //40.148802, 44.437512
     });
 
     this._projectService.postUser(this.user)
