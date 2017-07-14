8886d1e0e3ec1122012498de94abd422d99d5c73
ARTUR <artur.tovmasyan777@gmail.com>
2017-07-14 11:35:02 +0400

    fix in form validation

--- src/app/control-messages/control-messages.component.ts
+++ src/app/control-messages/control-messages.component.ts
@@ -14,7 +14,7 @@ export class ControlMessagesComponent  {
   get errorMessage() {
     for (const propertyName in this.control.errors) {
 
-      if (this.control.errors.hasOwnProperty(propertyName)) {
+      if (this.control.errors.hasOwnProperty(propertyName) && this.control.dirty) {
         return ValidationService.getValidatorErrorMessage(propertyName, this.control.errors[propertyName]);
       }
     }
